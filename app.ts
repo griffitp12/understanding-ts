dcc86e3b34e2e690736885ae62a2720e848dce1d
Peter <griffitp12@gmail.com>
2021-08-18 12:45:19 +0900

    toe wetting

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string): number | string {
+  if (showResult) {
+    console.log(`${phrase} ${n1 + n2}`);
+  } else {
+    return `${phrase} ${n1 + n2}`;
+  }
+}
+
+const number1: number = 3;
+const number2: number = 4.3;
+const printResult = true;
+const resultPhrase = "Result is:"
+
+add(number1, number2, printResult, resultPhrase);
