541182d5d4ca7dd2e1b2a59bfba5f8f3d76c2d15
Peter <griffitp12@gmail.com>
2021-08-19 11:35:51 +0900

    tuples and enums

--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
-const person = {
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const person/* : {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} */ = {
     name: "Pete",
     age: 37,
-    hobbies: ["running", "games"]
+    hobbies: ["running", "games"],
+    role: Role.ADMIN
 }
 
 console.log(person.name)
