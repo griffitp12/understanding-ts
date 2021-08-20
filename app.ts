021381b73a04856ef7b127d1b3fb2f8ee06f0ec7
Peter <griffitp12@gmail.com>
2021-08-20 11:20:33 +0900

    while and never

--- app.ts
+++ app.ts
@@ -1,26 +1,18 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5; 
+userInput = "pete";
+
+if(typeof userInput === 'string') {
+    userName = userInput;
 }
 
-function printResult(num: number) {
-  console.log("result " + num);
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+    /* while (true) {} */
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2
-    cb(result)
-}
+generateError("an error!", 500)
 
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-/* combineValues = printResult; */
-/* combineValues = 5; */
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-})
