aba0c86070917bc5a25d0ef364430e5ece8cd256
Peter <griffitp12@gmail.com>
2021-08-23 13:28:09 +0900

    public, private, inheritance, protected

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,57 @@
 class Department {
-  name: string;
-  private employees: string[] = []
-  constructor(n: string) {
-    this.name = n;
+  protected employees: string[] = [];
+  constructor(private name: string, private readonly id: string) {}
+  describeBasics() {
+    console.log(`Department (${this.id}): ${this.name}`);
   }
   addEmployee(employee: string) {
-      this.employees.push(employee)
+    this.employees.push(employee);
   }
-  describe() {
-      console.log("Department: " + this.name )
+  listEmployees() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
-
 }
 
-let program = new Department('Program');
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+  }
+  addEmployee(name: string) {
+      if (name === "Mike") {
+          return
+      } 
+      this.employees.push(name)
+  }
+}
+class ProgramDepartment extends Department {
+  constructor(id: string, private inSession: boolean) {
+    super(id, 'Program');
+  }
+  toggleInSession() {
+    this.inSession = !this.inSession;
+  }
+  checkIfInSession() {
+    if (this.inSession) {
+      console.log('Program is in session');
+    } else {
+      console.log('Program is not in session');
+    }
+  }
+}
 
-console.log(program.describe());
+const program = new ProgramDepartment('1', false);
+program.addEmployee('Pete');
+program.addEmployee('Mike');
+
+program.describeBasics();
+program.listEmployees();
+
+program.checkIfInSession()
+
+const IT = new ITDepartment('2', ['Pete']);
+IT.addEmployee('Karl');
+IT.addEmployee('Mike')
+
+IT.describeBasics();
+IT.listEmployees();
