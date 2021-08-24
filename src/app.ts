f5e75094ff5c7ef5a6a5a9f23d029113816ac6e3
Peter <griffitp12@gmail.com>
2021-08-24 11:44:21 +0900

    interfaces as functions

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,38 @@
-interface Greetable {
-    readonly name: string;
-    greet(phrase: string): void
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+ (a: number, b: number): number;
+}    
+
+let add2: AddFn
+
+add2 = (n1: number, n2:number) => {
+    return n1 + n2
+}
+
+interface Named {
+  readonly name: string;
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-    name: string;
-    age = 37
+  name: string;
+  age = 37;
 
-    constructor(n:string) {
-        this.name = n
-    }
+  constructor(n: string) {
+    this.name = n;
+  }
 
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name)
-    }
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  }
 }
-let user1: Greetable
 
-user1 = new Person("Pete")
-user1.greet("heya")
+let user1: Greetable;
 
-console.log(user1)
+user1 = new Person('Pete');
+user1.greet('heya');
+
+console.log(user1);
