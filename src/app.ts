b4e487ce59bb78d8aff5c1ce8b008d1e4105f73c
Peter <griffitp12@gmail.com>
2021-08-24 12:24:16 +0900

    optional params and props

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 // type AddFn = (a: number, b: number) => number;
 interface AddFn {
- (a: number, b: number): number;
-}    
-
-let add2: AddFn
-
-add2 = (n1: number, n2:number) => {
-    return n1 + n2
+  (a: number, b: number): number;
 }
 
+let add2: AddFn;
+
+add2 = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -18,21 +19,27 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 37;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log(phrase + '!');
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Pete');
+user1 = new Person();
 user1.greet('heya');
 
 console.log(user1);
