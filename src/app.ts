aedc5ad74f8c700ae559122f7e9dc28484a007fd
Peter <griffitp12@gmail.com>
2021-08-24 09:10:59 +0900

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,45 @@ class Department {
     console.log(this.employees.length);
     console.log(this.employees);
   }
-}
+} /*  */
+
+type Computer = { serialNo: string; purchaseYear: number };
 
 class ITDepartment extends Department {
-  constructor(id: string, public admins: string[]) {
+  private lastComputer: Computer;
+
+  get mostRecentComputer() {
+    return this.lastComputer;
+  }
+
+  set mostRecentComputer(value: Computer) {
+    this.addComputer(value);
+  }
+
+  constructor(
+    id: string,
+    public admins: string[],
+    private computers: Computer[]
+  ) {
     super(id, 'IT');
+    this.lastComputer = computers[0];
   }
   addEmployee(name: string) {
-      if (name === "Mike") {
-          return
-      } 
-      this.employees.push(name)
+    if (name === 'Mike') {
+      return;
+    }
+    this.employees.push(name);
+  }
+  addComputer(computer: Computer) {
+    this.computers.push(computer);
+    this.lastComputer = computer;
+  }
+  listComputers() {
+    console.log(this.computers);
   }
 }
-class ProgramDepartment extends Department {
+
+/* class ProgramDepartment extends Department {
   constructor(id: string, private inSession: boolean) {
     super(id, 'Program');
   }
@@ -47,11 +72,17 @@ program.addEmployee('Mike');
 program.describeBasics();
 program.listEmployees();
 
-program.checkIfInSession()
+program.checkIfInSession(); */
 
-const IT = new ITDepartment('2', ['Pete']);
+const IT = new ITDepartment('2', ['Pete'], []);
 IT.addEmployee('Karl');
-IT.addEmployee('Mike')
+IT.addEmployee('Mike');
 
 IT.describeBasics();
 IT.listEmployees();
+IT.listComputers();
+
+const macbook1: Computer = { serialNo: 'chf6nch', purchaseYear: 2021 };
+IT.mostRecentComputer = macbook1;
+IT.listComputers();
+console.log(IT.mostRecentComputer);
