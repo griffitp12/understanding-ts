2af230e0daf638bceccd4f3088356c45e53fab93
Peter <griffitp12@gmail.com>
2021-08-30 12:08:18 +0900

    keyof

--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,27 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: 'pete' }, { age: 37 })
 console.log(mergedObj.age)
+
+interface lengthy {
+    length: number
+}
+
+function countAndDescribe<T extends lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element'
+    } else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements'
+    }
+    return [element, descriptionText];
+}
+
+const result1 = countAndDescribe([2,1])
+console.log(result1)
+
+function extractAndConvert<T extends object, U extends keyof T>(obj:T, key:U) {
+    return 'Value: ' + obj[key]
+}
+
+let result2 = extractAndConvert({name: 'pete'}, 'name')
+console.log(result2)
