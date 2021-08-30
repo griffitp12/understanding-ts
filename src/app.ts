c4666b6d767b7356036fcb27088ed0661549c91e
Peter <griffitp12@gmail.com>
2021-08-30 15:05:30 +0900

    utility types

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,21 @@ objStorage.removeItem({ name: 'pete' });
 
 console.log(objStorage.getItems());
  */
+
+interface CourseGoal {
+  title: string,
+  description: string,
+  completeUntil: Date
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+ let courseGoal: Partial<CourseGoal> = {};
+ courseGoal.title = title;
+ courseGoal.description = description;
+ courseGoal.completeUntil = date;
+ return courseGoal as CourseGoal
+}
+
+const names: Readonly<string[ ]> = ['pete', 'shin']
+
+names.push('mone')
